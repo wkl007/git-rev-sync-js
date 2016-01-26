bdb180b3bf029cc9e4bc5fee0434fb2707b3abeb
kurttheviking <kurttheviking@outlook.com>
2016-01-26 09:35:00 -0800

    use ;

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var fs = require('graceful-fs')
-var path = require('path')
-var childProcess = require('child_process')
-var shell = require('shelljs')
+var fs = require('graceful-fs');
+var path = require('path');
+var childProcess = require('child_process');
+var shell = require('shelljs');
 
-var HAS_NATIVE_EXECSYNC = childProcess.hasOwnProperty('spawnSync')
-var PATH_SEP = path.sep
-var RE_BRANCH = /^ref: refs\/heads\/(.*)\n/
+var HAS_NATIVE_EXECSYNC = childProcess.hasOwnProperty('spawnSync');
+var PATH_SEP = path.sep;
+var RE_BRANCH = /^ref: refs\/heads\/(.*)\n/;
 
 function _command(cmd, args) {
   var result;
@@ -32,69 +32,73 @@ function _command(cmd, args) {
 }
 
 function _getGitDirectory(start) {
-  start = start || module.parent.filename
+  start = start || module.parent.filename;
+
   if (typeof start === 'string') {
-    start = start.split(PATH_SEP)
+    start = start.split(PATH_SEP);
   }
-  start.pop()
-  var testPath = path.resolve(start.join(PATH_SEP), '.git')
+
+  start.pop();
+
+  var testPath = path.resolve(start.join(PATH_SEP), '.git');
   if (fs.existsSync(testPath)) {
-    return testPath
+    return testPath;
   }
 
   if (!start.length) {
     throw new Error('[git-rev-sync] no git repository found');
   }
 
-  return _getGitDirectory(start)
+  return _getGitDirectory(start);
 }
 
-function branch (dir) {
+function branch(dir) {
   var gitDir = _getGitDirectory(dir);
 
-  var head = fs.readFileSync(path.resolve(gitDir, 'HEAD'), 'utf8')
-  var b = head.match(RE_BRANCH)
+  var head = fs.readFileSync(path.resolve(gitDir, 'HEAD'), 'utf8');
+  var b = head.match(RE_BRANCH);
 
   if (b) {
-    return b[1]
+    return b[1];
   }
 
-  return 'Detatched: ' + head.trim()
+  return 'Detatched: ' + head.trim();
 }
 
 function long(dir) {
-  var b = branch(dir)
+  var b = branch(dir);
 
   if (/Detatched: /.test(b)) {
-    return b.substr(11)
+    return b.substr(11);
   }
 
-  var gitDir = _getGitDirectory(dir)
-  var refsFilePath = path.resolve(gitDir, 'refs', 'heads', b)
+  var gitDir = _getGitDirectory(dir);
+  var refsFilePath = path.resolve(gitDir, 'refs', 'heads', b);
   var ref;
 
   if (fs.existsSync(refsFilePath)) {
-    ref = fs.readFileSync(refsFilePath, 'utf8')
+    ref = fs.readFileSync(refsFilePath, 'utf8');
   }
+
   else {
     // If there isn't an entry in /refs/heads for this branch, it may be that
     // the ref is stored in the packfile (.git/packed-refs). Fall back to
     // looking up the hash here.
-    var refToFind = path.join('refs', 'heads', b)
-    var packfileContents = fs.readFileSync(path.resolve(gitDir, 'packed-refs'), 'utf8')
-    var packfileRegex = new RegExp('(.*) ' + refToFind)
-    ref = packfileRegex.exec(packfileContents)[1]
+    var refToFind = path.join('refs', 'heads', b);
+    var packfileContents = fs.readFileSync(path.resolve(gitDir, 'packed-refs'), 'utf8');
+    var packfileRegex = new RegExp('(.*) ' + refToFind);
+    ref = packfileRegex.exec(packfileContents)[1];
   }
 
-  return ref.trim()
+  return ref.trim();
 }
 
 function short(dir) {
-  return long(dir).substr(0, 7)
+  return long(dir).substr(0, 7);
 }
 
 function message() {
-  return _command('git', ['log', '-1', '--pretty=%B'])
+  return _command('git', ['log', '-1', '--pretty=%B']);
 }
 
 function tag() {
@@ -106,15 +110,15 @@ function count() {
 }
 
 function log() {
-  throw new Error('not implemented')
+  throw new Error('not implemented');
 }
 
 module.exports = {
   branch : branch,
+  count: count,
   log : log,
   long : long,
   message : message,
   short : short,
-  tag : tag,
-  count: count
+  tag : tag
 };
