9153de11b6b395e247ad3fff76562fcbeb66830e
kurttheviking <kurttheviking@outlook.com>
2016-04-01 15:06:20 -0700

    allow for example run with filePath

--- examples/extended.js
+++ examples/extended.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var git = require('../');
+var otherDir = '';
 
 console.log('git.short() => ' + git.short());
 console.log('git.long() => ' + git.long());
@@ -8,3 +9,9 @@ console.log('git.branch() => ' + git.branch());
 console.log('git.message() => ' + git.message());
 console.log('git.tag() => ' + git.tag());
 console.log('git.count() => ' + git.count());
+
+if (otherDir) {
+  console.log('git.short(' + otherDir + ') => ' + git.short(otherDir));
+  console.log('git.long(' + otherDir + ') => ' + git.long(otherDir));
+  console.log('git.branch(' + otherDir + ') => ' + git.branch(otherDir));
+}
